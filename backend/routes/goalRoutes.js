973aaf9cc4e9174549b779d86c7dd05d56eb2dc6
Dan Shawl <danielrshawl@gmail.com>
2022-08-10 16:43:33 -0400

    Set up controller functions and refactored goalRoutes file

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,21 +1,25 @@
 const express = require('express'); // common js syntax
 const router = express.Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require('../controllers/goalController'); //  bring in from controller
 
-router.get('/', (req, res) => {
-  // res.send('Get goals');
-  res.status(200).json({ message: 'Get Goals' }); // usually json
-}); //  get is the request we want to listen for
+// This route is replaced with below with controller functions
+// router.get('/', (req, res) => {
+//   // res.send('Get goals');
+//   res.status(200).json({ message: 'Get Goals' }); // usually json
+// }); //  get is the request we want to listen for
 
-router.post('/', (req, res) => {
-  res.status(200).json({ message: 'Set Goal' });
-});
+//  These routes are refactored with below
+// router.get('/', getGoals);
+// router.post('/', setGoal);
+// router.put('/:id', updateGoal);
+// router.delete('/:id', deleteGoal);
 
-router.put('/:id', (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
-});
-
-router.delete('/:id', (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
-});
+router.route('/').get(getGoals).post(setGoal);
+router.route('/:id').delete(deleteGoal).put(updateGoal);
 
 module.exports = router;
